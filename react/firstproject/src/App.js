9be1f64e59013d474403f47c360c2abf9fa4d5f0
rayhonfk <rayhonfazilova75@gmail.com>
2023-03-24 10:12:16 +0500

    javascript code

---
+++ react/firstproject/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Event from './Event'
+
+function App() {
+  return (
+    <div>
+      <Event/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/firstproject/src/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import './style.css'
+function Card(){
+  return (
+    <div className='wrapper'>
+        <img style={width:"400px",height:"300px"}              
+    </div>
+  )
+}
